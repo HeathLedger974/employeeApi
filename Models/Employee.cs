fee4bd7667d91b9828ffbb507bf5cb7134891544
joker1785 <veerabalan13020212gmail.com>
2025-04-30 18:06:26 +0530

    CRUD code

---
+++ Models/Employee.cs
@@ -0,0 +1,11 @@
+namespace EmployeeApi.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
